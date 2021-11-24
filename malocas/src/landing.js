6ee769be9e01181f81003e4e94c64fb399d60b21
Damt26 <damttrabajos@gmail.com>
2021-11-24 16:40:51 -0500

    Maloca 0.2.0

--- malocas/src/landing.js
+++ malocas/src/landing.js
@@ -13,16 +13,12 @@ function LandingPage() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
-                            <li className="nav-item"><a className="nav-link me-lg-3" href="#features">Features</a></li>
-                            <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Download</a></li>
+                            <li className="nav-item"><a className="nav-link me-lg-3" href="#features">Inicio</a></li>
+                            <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Servicios</a></li>
+                            <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Información</a></li>
+                            <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Ingreso</a></li>
+                            <li className="nav-item"><a role="button" className="btn btn-success me-lg-3" href="#download" >Registro</a></li>
                         </ul>
-                        <button className="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal"
-                            data-bs-target="#feedbackModal">
-                            <span className="d-flex align-items-center">
-                                <i className="bi-chat-text-fill me-2"></i>
-                                <span className="small">Send Feedback</span>
-                            </span>
-                        </button>
                     </div>
                 </div>
             </nav>
@@ -31,13 +27,11 @@ function LandingPage() {
                     <div className="row gx-5 align-items-center">
                         <div className="col-lg-6">
                             <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                                <h1 className="display-1 lh-1 mb-3">Showcase your app beautifully.</h1>
-                                <p className="lead fw-normal text-muted mb-5">Launch your mobile app landing page faster with this
-                                    free, open source theme from Start Bootstrap!</p>
+                                <h5> Bienvenidos a Agricola Las Malocas Ltda </h5>
+                                <h1 className="display-1 lh-1 mb-3"><strong>Calidad para tus cultivos</strong></h1>
+                                <p className="lead fw-normal text-muted mb-5">Realización de toda clase de actividades agropecuarias y/o agroindustriales en terrenos propios o ajenos.</p>
                                 <div className="d-flex flex-column flex-lg-row align-items-center">
-                                    <a className="me-lg-3 mb-4 mb-lg-0" href="#!"><img className="app-badge"
-                                        src="assets/img/google-play-badge.svg" alt="..." /></a>
-                                    <a href="#!"><img className="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
+                                    <a role="button" className="btn btn-success me-lg-3 mb-4 mb-lg-0" href="#!">Ingresar a muestrame </a>
                                 </div>
                             </div>
                         </div>
@@ -62,6 +56,7 @@ function LandingPage() {
                                     <circle cx="50" cy="50" r="50"></circle>
                                 </svg>
                                 <div className="device-wrapper">
+                                <img src="./src/assets/img/scythe.png" alt="" className="w50 h50"></img>
 
                                 </div>
                             </div>
@@ -69,12 +64,11 @@ function LandingPage() {
                     </div>
                 </div>
             </header>
-            <aside className="text-center bg-gradient-primary-to-secondary">
+            <aside className="text-center bg-success">
                 <div className="container px-5">
                     <div className="row gx-5 justify-content-center">
                         <div className="col-xl-8">
-                            <div className="h2 fs-1 text-white mb-4">"An intuitive solution to a common problem that we all face,
-                                wrapped up in a single app!"</div>
+                            <div className="h5 fs-1 text-white mb-4">"La web de Agricola Las Malocas Ltda está clasificada para brindar a través de un sistema la información de la siembra y recolección de cultivos."</div>
                             <img src="assets/img/tnw-logo.svg" alt="..." style={{ height: '3rem' }} />
                         </div>
                     </div>
@@ -174,7 +168,7 @@ function LandingPage() {
                             <br />
                             Start building.
                         </h2>
-                        
+
                     </div>
                 </div>
             </section>
@@ -200,60 +194,6 @@ function LandingPage() {
                     </div>
                 </div>
             </footer>
-            <div className="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header bg-gradient-primary-to-secondary p-4">
-                            <h5 className="modal-title font-alt text-white" id="feedbackModalLabel">Send feedback</h5>
-                            <button className="btn-close btn-close-white" type="button" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body border-0 p-4">
-                            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                                <div className="form-floating mb-3">
-                                    <input className="form-control" id="name" type="text" placeholder="Enter your name..."
-                                        data-sb-validations="required" />
-                                    <label for="name">Full name</label>
-                                    <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                                </div>
-                                <div className="form-floating mb-3">
-                                    <input className="form-control" id="email" type="email" placeholder="name@example.com"
-                                        data-sb-validations="required,email" />
-                                    <label for="email">Email address</label>
-                                    <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                    <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                                </div>
-                                <div className="form-floating mb-3">
-                                    <input className="form-control" id="phone" type="tel" placeholder="(123) 456-7890"
-                                        data-sb-validations="required" />
-                                    <label for="phone">Phone number</label>
-                                    <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.
-                                    </div>
-                                </div>
-                                <div className="form-floating mb-3">
-                                    <textarea className="form-control" id="message" type="text"
-                                        placeholder="Enter your message here..." style={{height: '10rem'}}
-                                        data-sb-validations="required"></textarea>
-                                    <label for="message">Message</label>
-                                    <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.
-                                    </div>
-                                </div>
-                                <div className="d-none" id="submitSuccessMessage">
-                                    <div className="text-center mb-3">
-                                        <div className="fw-bolder">Form submission successful!</div>
-                                        To activate this form, sign up at
-                                        <br />
-                                        <a
-                                            href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                    </div>
-                                </div>
-                                <div className="d-grid"><button className="btn btn-primary rounded-pill btn-lg disabled"
-                                    id="submitButton" type="submit">Submit</button></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
             <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
             <script src="js/scripts.js"></script>

83155d7181f761819e78eb41cad7a94b1ebb5e89
Damt26 <damttrabajos@gmail.com>
2021-11-24 21:46:50 -0500

    Malocas 1.0.0

--- malocas/src/landing.js
+++ malocas/src/landing.js
@@ -1,11 +1,22 @@
 import './styles.css';
+import campo3 from './assets/img/campo3.jpg';
+import campo4 from './assets/img/campo4.jpg';
+import campo6 from './assets/img/campo6.jpg';
+import azada from './assets/img/scythe.png';
+import siembra2 from './assets/img/siembra2.png';
+import siembra from './assets/img/siembra.png';
+import granja from './assets/img/granja.png';
+import ecologia from './assets/img/ecologia.png';
+
+
 
 function LandingPage() {
     return (
         <div className="LandingPage">
             <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
                 <div className="container px-5">
-                    <a className="navbar-brand fw-bold" href="#page-top">Start Bootstrap</a>
+                    <a className="navbar-brand py-0" href="#page-top"><img src={azada} alt="azada" style={{ height: '2em', weight: '2em' }} /></a>
+                    <h4 className="fw-bold">Malocas</h4>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                         aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         Menu
@@ -28,48 +39,25 @@ function LandingPage() {
                         <div className="col-lg-6">
                             <div className="mb-5 mb-lg-0 text-center text-lg-start">
                                 <h5> Bienvenidos a Agricola Las Malocas Ltda </h5>
-                                <h1 className="display-1 lh-1 mb-3"><strong>Calidad para tus cultivos</strong></h1>
+                                <h1 className="display-1 lh-1 mb-3 fw-bold" style={{ color: '#034303' }}>Calidad para tus cultivos</h1>
                                 <p className="lead fw-normal text-muted mb-5">Realización de toda clase de actividades agropecuarias y/o agroindustriales en terrenos propios o ajenos.</p>
                                 <div className="d-flex flex-column flex-lg-row align-items-center">
-                                    <a role="button" className="btn btn-success me-lg-3 mb-4 mb-lg-0" href="#!">Ingresar a muestrame </a>
+                                    <a role="button" className="btn btn-success me-lg-3 mb-4 mb-lg-0" href="#!">Ingresar a Malocas </a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-6">
-                            <div className="masthead-device-mockup">
-                                <svg className="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                                    <defs>
-                                        <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                                            <stop className="gradient-start-color" offset="0%"></stop>
-                                            <stop className="gradient-end-color" offset="100%"></stop>
-                                        </linearGradient>
-                                    </defs>
-                                    <circle cx="50" cy="50" r="50"></circle>
-                                </svg><svg className="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03"
-                                        transform="translate(120.42 -49.88) rotate(45)"></rect>
-                                    <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03"
-                                        transform="translate(-49.88 120.42) rotate(-45)"></rect>
-                                </svg><svg className="shape-2 d-none d-sm-block" viewBox="0 0 100 100"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="50" cy="50" r="50"></circle>
-                                </svg>
-                                <div className="device-wrapper">
-                                <img src="./src/assets/img/scythe.png" alt="" className="w50 h50"></img>
-
-                                </div>
-                            </div>
+                            <img src={campo6} alt="" style={{ height: '25rem', width: '25rem', borderRadius: '18px' }}></img>
                         </div>
                     </div>
                 </div>
             </header>
             <aside className="text-center bg-success">
-                <div className="container px-5">
-                    <div className="row gx-5 justify-content-center">
+                <div className="container-fluid px-5 pt-0">
+                    <div className="row gx-6 justify-content-center">
                         <div className="col-xl-8">
-                            <div className="h5 fs-1 text-white mb-4">"La web de Agricola Las Malocas Ltda está clasificada para brindar a través de un sistema la información de la siembra y recolección de cultivos."</div>
-                            <img src="assets/img/tnw-logo.svg" alt="..." style={{ height: '3rem' }} />
+                            <p className="fs-4 text text-white mb-4">"La web de Agricola Las Malocas Ltda está clasificada para brindar a través de un sistema la información de la siembra y recolección de cultivos."</p>
+                            <a role="button" className="btn btn-success btn-lg mb-4 mb-lg-0 w-25" href="#!">Registrarse </a>
                         </div>
                     </div>
                 </div>
@@ -82,84 +70,59 @@ function LandingPage() {
                                 <div className="row gx-5">
                                     <div className="col-md-6 mb-5">
                                         <div className="text-center">
-                                            <i className="bi-phone icon-feature text-gradient d-block mb-3"></i>
-                                            <h3 className="font-alt">Device Mockups</h3>
-                                            <p className="text-muted mb-0">Ready to use HTML/CSS device mockups, no Photoshop
-                                                required!</p>
+                                        <img src={siembra2} alt="" style={{ height: '5rem', width: '5rem'}}></img>
+                                            <h3 className="font-alt">Lorem</h3>
+                                            <p className="text-muted mb-0">Lorem ipsum dolor lorem, consectetur adipiscing elit</p>
                                         </div>
                                     </div>
                                     <div className="col-md-6 mb-5">
                                         <div className="text-center">
-                                            <i className="bi-camera icon-feature text-gradient d-block mb-3"></i>
-                                            <h3 className="font-alt">Flexible Use</h3>
-                                            <p className="text-muted mb-0">Put an image, video, animation, or anything else in the
-                                                screen!</p>
+                                        <img src={granja} alt="" style={{ height: '5rem', width: '5rem'}}></img>
+                                            <h3 className="font-alt">Lorem</h3>
+                                            <p className="text-muted mb-0">Lorem ipsum dolor lorem, consectetur adipiscing elit</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-6 mb-5 mb-md-0">
                                         <div className="text-center">
-                                            <i className="bi-gift icon-feature text-gradient d-block mb-3"></i>
-                                            <h3 className="font-alt">Free to Use</h3>
-                                            <p className="text-muted mb-0">As always, this theme is free to download and use for any
-                                                purpose!</p>
+                                        <img src={siembra} alt="" style={{ height: '5rem', width: '5rem'}}></img>
+                                            <h3 className="font-alt">Lorem</h3>
+                                            <p className="text-muted mb-0">Lorem ipsum dolor lorem, consectetur adipiscing elit</p>
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <div className="text-center">
-                                            <i className="bi-patch-check icon-feature text-gradient d-block mb-3"></i>
-                                            <h3 className="font-alt">Open Source</h3>
-                                            <p className="text-muted mb-0">Since this theme is MIT licensed, you can use it
-                                                commercially!</p>
+                                            <img src={ecologia} alt="" style={{ height: '5rem', width: '5rem'}}></img>
+                                            <h3 className="font-alt">Lorem</h3>
+                                            <p className="text-muted mb-0">Lorem ipsum dolor lorem, consectetur adipiscing elit</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-4 order-lg-0">
-                            <div className="features-device-mockup">
-                                <svg className="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                                    <defs>
-                                        <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                                            <stop className="gradient-start-color" offset="0%"></stop>
-                                            <stop className="gradient-end-color" offset="100%"></stop>
-                                        </linearGradient>
-                                    </defs>
-                                    <circle cx="50" cy="50" r="50"></circle>
-                                </svg><svg className="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03"
-                                        transform="translate(120.42 -49.88) rotate(45)"></rect>
-                                    <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03"
-                                        transform="translate(-49.88 120.42) rotate(-45)"></rect>
-                                </svg><svg className="shape-2 d-none d-sm-block" viewBox="0 0 100 100"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="50" cy="50" r="50"></circle>
-                                </svg>
-                                <div className="device-wrapper">
-                                </div>
-                            </div>
+                        <img src={campo3} alt="" style={{ height: '25rem', width: '25rem', borderRadius: '18px' }}></img>
                         </div>
                     </div>
                 </div>
             </section>
-            <section className="bg-light">
+            <aside className="bg-success">
                 <div className="container px-5">
                     <div className="row gx-5 align-items-center justify-content-center justify-content-lg-between">
                         <div className="col-12 col-lg-5">
-                            <h2 className="display-4 lh-1 mb-4">Enter a new age of web design</h2>
-                            <p className="lead fw-normal text-muted mb-5 mb-lg-0">This section is perfect for featuring some
+                            <p className="fs-2 fw-bold lh-1 text-white mb-4">Malocas Ltda</p>
+                            <p className="lead fw-normal text-white mb-5 mb-lg-0">This section is perfect for featuring some
                                 information about your application, why it was built, the problem it solves, or anything else!
                                 There's plenty of space for text here, so don't worry about writing too much.</p>
                         </div>
                         <div className="col-sm-8 col-md-6">
-                            <div className="px-5 px-sm-0"><img className="img-fluid rounded-circle"
-                                src="https://source.unsplash.com/u8Jn2rzYIps/900x900" alt="..." /></div>
+                            <img className="img-fluid rounded-circle"
+                                src={campo4} alt="..." width="40vw" height="40vh"/>
                         </div>
                     </div>
                 </div>
-            </section>
+            </aside>
             <section className="cta">
                 <div className="cta-content">
                     <div className="container px-5">
@@ -176,6 +139,7 @@ function LandingPage() {
                 <div className="container px-5">
                     <h2 className="text-center text-white font-alt mb-4">Get the app now!</h2>
                     <div className="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                        
                         <a className="me-lg-3 mb-4 mb-lg-0" href="#!"><img className="app-badge" src="assets/img/google-play-badge.svg"
                             alt="..." /></a>
                         <a href="#!"><img className="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
